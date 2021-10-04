8848c582a16918382065cb564638a0fc34787e89
Pawel Zini <83308656+pziniewicz@users.noreply.github.com>
2021-10-04 17:29:12 +0200

    entity Donation created. DonationRepository created. DonationService created

---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,42 @@
+package pl.coderslab.charity.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    @OneToOne
+    private Institution institution;
+
+    private String street;
+
+    private String city;
+
+    private String zipcode;
+
+    private LocalDate pickUpDate;
+
+    private LocalTime pickUpTime;
+
+    private String pickUpComment;
+}
