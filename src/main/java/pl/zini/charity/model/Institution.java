b833d09b6014a171135f8c6f214b2db3c06c6d2e
Pawel Zini <83308656+pziniewicz@users.noreply.github.com>
2021-10-06 12:06:44 +0200

    BaseClass to Institution implemented

--- src/main/java/pl/zini/charity/model/Institution.java
+++ src/main/java/pl/zini/charity/model/Institution.java
@@ -6,9 +6,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Getter
 @Setter
